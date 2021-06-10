39d8db09ebe8675bbffcf9e774321bc39d79db3f
Amr Aboelela <amraboelela@gmail.com>
2021-06-10 14:58:58 -0700

    UniversalLocator.swift

--- Sources/UniversalLocator/UniversalLocator.swift
+++ Sources/UniversalLocator/UniversalLocator.swift
@@ -1,3 +1,23 @@
-struct UniversalLocator {
-    var text = "Hello, World!"
+
+public let universalLocator = UniversalLocator()
+
+public class UniversalLocator {
+    
+    let codes = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "B", "C", "D", "F",
+    "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Z"]
+    
+    func codeFor(number: Double) -> String {
+        let diff = number - (-180.0)
+        let ratio = diff / 360.0
+        var index = Int(ratio * Double(codes.count))
+        if index >= codes.count {
+            index = codes.count - 1
+        }
+        let code = codes[index]
+        return code
+    }
+    
+    func codeFor(latitude: Double, logitude: Double) -> String {
+        return codeFor(number: latitude) + " " + codeFor(number: logitude)
+    }
 }
--- Tests/UniversalLocatorTests/UniversalLocatorTests.swift
+++ Tests/UniversalLocatorTests/UniversalLocatorTests.swift
@@ -3,9 +3,9 @@
 
     final class UniversalLocatorTests: XCTestCase {
         func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-            XCTAssertEqual(UniversalLocator().text, "Hello, World!")
+            XCTAssertEqual(universalLocator.codeFor(latitude: -180, logitude: -180), "0 0")
+            XCTAssertEqual(universalLocator.codeFor(latitude: 0, logitude: 0), "H H")
+            XCTAssertEqual(universalLocator.codeFor(latitude: 180, logitude: 180), "Z Z")
+            XCTAssertEqual(universalLocator.codeFor(latitude: 30.099999999999994, logitude: 31.39999999999999), "KLH00 N0GZZ")
         }
     }
