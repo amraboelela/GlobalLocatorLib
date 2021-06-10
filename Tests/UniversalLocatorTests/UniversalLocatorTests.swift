8b1e39812cda277fcb292d60220b9866a95bc62d
Amr Aboelela <amraboelela@gmail.com>
2021-06-10 16:48:47 -0700

    longitude

--- Tests/UniversalLocatorTests/UniversalLocatorTests.swift
+++ Tests/UniversalLocatorTests/UniversalLocatorTests.swift
@@ -3,9 +3,10 @@
 
     final class UniversalLocatorTests: XCTestCase {
         func testExample() {
-            XCTAssertEqual(universalLocator.codeFor(latitude: -180, logitude: -180), "0 0")
-            XCTAssertEqual(universalLocator.codeFor(latitude: 0, logitude: 0), "H H")
-            XCTAssertEqual(universalLocator.codeFor(latitude: 180, logitude: 180), "Z Z")
-            XCTAssertEqual(universalLocator.codeFor(latitude: 30.099999999999994, logitude: 31.39999999999999), "KLH00 N0GZZ")
+            XCTAssertEqual(universalLocator.codeFor(latitude: -180, longitude: -90), "0 0")
+            XCTAssertEqual(universalLocator.codeFor(latitude: 0, longitude: 0), "H H")
+            XCTAssertEqual(universalLocator.codeFor(latitude: 180, longitude: 90), "ZZZZZ ZZZZZ")
+            XCTAssertEqual(universalLocator.codeFor(latitude: 50.46944444444445, longitude: -104.68244444444444), "688SH RDBDG")
+            //XCTAssertEqual(universalLocator.codeFor(latitude: 30.099999999999994, logitude: 31.39999999999999), "KLH00 N0GZZ")
         }
     }
